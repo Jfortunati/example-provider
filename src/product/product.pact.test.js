ed20ea9d4bb71a59a1f1441354ab1b9408f50bb4
Jonathan.Fortunati@smartbear.com <jonathan.fortunati@smartbear.com>
2022-03-24 11:20:33 -0400

    Changed pending to false

--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({path:__dirname+'/./../../.env'})
 const { Verifier } = require('@pact-foundation/pact');
 const controller = require('./product.controller');
 const Product = require('./product');
