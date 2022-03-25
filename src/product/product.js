5c160b190cb28e62b37c3087377ef2bca6406bfa
Jonathan.Fortunati@smartbear.com <jonathan.fortunati@smartbear.com>
2022-03-25 16:58:12 -0400

    tests passing on master and WIP

--- src/product/product.js
+++ src/product/product.js
@@ -1,9 +1,10 @@
 class Product {
-    constructor(id, type, name, version) {
+    constructor(id, type, name, price, color) {
         this.id=id;
         this.type = type;
         this.name = name;
-        this.version = version;
+        this.price = price;
+        this.color = color;
     }
 }
 
--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -35,23 +35,23 @@ describe("Pact Verification", () => {
       //consumerVersionSelectors: [{ tag: 'master', latest: true } ],
       pactBrokerUrl: process.env.PACT_BROKER_BASE_URL,
       enablePending: true,
-      includeWipPactsSince: undefined
+      includeWipPactsSince: "2020-01-01"
     }
 
     const stateHandlers = {
       "products exists": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", 29.0, "red")]
         ]);
       },
       "products exist": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", 29.0, "red")]
         ]);
       },
       "a product with ID 10 exists": () => {
         controller.repository.products = new Map([
-          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
+          ["10", new Product("10", "CREDIT_CARD", "28 Degrees", 29.0, "red")]
         ]);
       },
       "a product with ID 11 does not exist": () => {
